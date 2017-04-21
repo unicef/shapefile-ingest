025fc0d571c927c794fab5e44208d5486cb47cca
mikefab <mikefabrikant@gmail.com>
2017-04-21 16:53:56 -0400

    Geojson setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 mkdir -p data/shapefiles
 mkdir -p data/zipfiles
 mkdir -p data/aggregations/human/processed
+mkdir -p data/geojson/gadm2-8
 
 check_file() {
   local dir="$1"
@@ -14,3 +15,4 @@ check_file 'data/shapefiles'
 check_file 'data/zipfiles'
 check_file 'data/aggregations/human'
 check_file 'data/aggregations/human/processed'
+check_file 'data/geojson/gadm2-8'
