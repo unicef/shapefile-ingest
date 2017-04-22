ebd477b90292a29011dc9694fd9ec046169f1b10
mikefab <mikefabrikant@gmail.com>
2017-04-21 23:52:47 -0400

    topo setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ mkdir -p data/shapefiles
 mkdir -p data/zipfiles
 mkdir -p data/aggregations/human/processed
 mkdir -p data/geojson/gadm2-8
+mkdir -p data/topojson/gadm2-8
 
 check_file() {
   local dir="$1"
@@ -16,3 +17,4 @@ check_file 'data/zipfiles'
 check_file 'data/aggregations/human'
 check_file 'data/aggregations/human/processed'
 check_file 'data/geojson/gadm2-8'
+check_file 'data/topojson/gadm2-8'
