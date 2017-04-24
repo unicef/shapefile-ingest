5eac05d8ea0a97b288b1aad7d2789119b6ba9c77
mikefab <mikefabrikant@gmail.com>
2017-04-24 19:04:04 -0400

    No country directory for geojson

--- shapefile_to_geojson.js
+++ shapefile_to_geojson.js
@@ -1,12 +1,30 @@
+// node shapefile_to_geojson -s gadm2-8
+var ArgumentParser = require('argparse').ArgumentParser;
 var bluebird = require('bluebird');
 var config = require('./config');
 var fs = require('fs');
-var shapefile_dir = config.shapefile_dir;
+
 var mkdirp = require('mkdirp');
 var ogr2ogr = require('ogr2ogr');
-var geojson_dir = config.geojson_dir;
 
+
+var parser = new ArgumentParser({
+  version: '0.0.1',
+  addHelp: true,
+  description: 'Aggregate a csv of airport by admin 1 and 2'
+});
+
+parser.addArgument(
+  ['-s', '--source'],
+  {help: 'Shapefile source. Example: gadm2-8'}
+);
+
+var args = parser.parseArgs();
+var source = args.source;
+var shapefile_dir = config.shapefile_dir + source + '/';
+var geojson_dir = config.geojson_dir + source;
 var shapefile_dirs = fs.readdirSync(shapefile_dir);
+
 bluebird.each(shapefile_dirs, country => {
   return prepare_to_geojsonize(country);
 }, {concurrency: 1})
@@ -18,17 +36,17 @@ bluebird.each(shapefile_dirs, country => {
  */
 function prepare_to_geojsonize(country_code) {
   return new Promise((resolve, reject) => {
-    mkdirp(geojson_dir + country_code, err => {
-      if (err){
-        console.log(err);
-        return reject(err);
-      }
+    // mkdirp(geojson_dir + country_code, err => {
+    //   if (err){
+    //     console.log(err);
+    //     return reject(err);
+    //   }
       // I'm just lazy.
       bluebird.each([0,1,2,3,4,5,6,7,8,9,10], level => {
         return geojsonize(country_code, level);
       }, {concurrency: 1})
       .then(resolve);
-    });
+    // });
   });
 };
 
@@ -41,6 +59,7 @@ function prepare_to_geojsonize(country_code) {
 function geojsonize(country_code, admin_level) {
   var input = shapefile_dir + country_code + '/' + country_code + '_adm' + admin_level + '.shp';
   var output = geojson_dir + '/' + country_code + '_' + admin_level + '.json';
+  console.log(input, output)
   return new Promise((resolve, reject) => {
     if (fs.existsSync(input)) {
       var ogr = ogr2ogr(input);
