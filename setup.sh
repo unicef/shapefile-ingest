a44aaf7cde29034f32ee96ccbb7706c4654b44d7
Ubuntu <mfabrikant@raster.tnhb0fuarrrufbuqhvnmkxkvbd.bx.internal.cloudapp.net>
2017-05-04 20:06:03 +0000

    Setup aegypti folders

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 mkdir -p data/shapefiles
 mkdir -p data/zipfiles
 mkdir -p data/aggregations/human/processed
+mkdir -p data/aggregations/aegypti/processed
 mkdir -p data/geojson/gadm2-8
 mkdir -p data/topojson/gadm2-8
 mkdir -p data/shapefiles/gadm2-8
@@ -17,6 +18,7 @@ check_file 'data/shapefiles'
 check_file 'data/zipfiles'
 check_file 'data/aggregations/human'
 check_file 'data/aggregations/human/processed'
+check_file 'data/aggregations/aegypti/processed'
 check_file 'data/geojson/gadm2-8'
 check_file 'data/topojson/gadm2-8'
 check_file 'data/shapefiles/gadm2-8'
