71a514522efdbb22595fe4e0e0dfcf4e6a29532b
mikefab <mikefabrikant@gmail.com>
2017-05-16 13:09:54 -0400

    Save tif to cloud

--- lib/fetch_and_process_raster.sh
+++ lib/fetch_and_process_raster.sh
@@ -6,34 +6,45 @@ if [[ "$1" =~ ^(irn|jor|tur|tkm)$ ]]; then
   upper="Asia-POP-1KM"
 fi
 
-if [ ! -f $2$3/$1.zip ]; then
+# Drop the table if it exists
+
+psql all_countries -c "DROP TABLE IF EXISTS pop;"
+
+if [ -d $4$1 ]; then
+  for file in $4$1\/*tif; do
+    tif=${file##*/}
+  done
+fi
+
+if [ ! $tif ]; then
+
 	wget "http://www.worldpop.org.uk/data/hdx/?dataset=$upper" -O $2$3/$1.zip
 	unzip -d $2$3/$1 $2$3/$1.zip
-        ls $2$3/$1
+  file=$2$3/$1/*.tif
+  if [ -f $file ]; then
+    # create ../../rasters/population/afg/
+    if [ ! -d $4$1 ]; then
+      mkdir $4$1
+    fi
+    echo "File exists! $file"
+    fbname=$(basename $file)
+    echo $fbname
+    echo $file
+    echo "____"
+    echo $2$3/$1*
+    cp $file $4$1/$fbname
+    echo "FFFFF"
+    echo $1
+    rm -rf $2$3/$1
+    rm -rf $2$3/$1/$1.zip
+    tif=$fbname
+  fi
 fi
 
-# Drop the table if it exists
-psql $1 -c "DROP TABLE IF EXISTS pop;"
+
 
 # Use EPSG:4326 SRS, tile into 100x100 squares, and create an index
-raster2pgsql -Y -s 4326 -t 100x100 -I $2$3/$1/*.tif pop | psql all_countries
+raster2pgsql -Y -s 4326 -t 100x100 -I $4$1\/$tif pop | psql all_countries
 
-
-
-file=$2$3/$1/*.tif
-
-if [ -f $file ]; then
-  # create ../../rasters/population/afg/
-  if [ ! -d $4$1 ]; then
-    mkdir $4$1
-  fi  
-  echo "File exists! $file"
-  fbname=$(basename $file)
-  echo $fbname
-  cp $file $4$1/$fbname
-fi
 # DO NOT REMOVE OR EDIT THIS LINE
-echo $file
-#SELECT gid, admin.name_1, admin.name_2, SUM((ST_SummaryStats(ST_Clip(rast, geom))).sum) FROM admin  LEFT JOIN pop ON ST_Intersects(admin.geom, pop.rast) GROUP BY gid;
-
-# SELECT dpto, wcolgen02_, SUM((ST_SummaryStats(ST_Clip(rast, wkb_geometry))).sum) FROM admins LEFT JOIN my_table ON ST_Intersects(admins.wkb_geometry, my_table.rast) GROUP BY wcolgen02_, dpto;'
+echo $4$1\/$tif
