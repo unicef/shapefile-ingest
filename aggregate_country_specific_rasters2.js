3fcd84e3809404b1780c1e01d96c1d8c04e3023c
mikefab <mikefabrikant@gmail.com>
2017-05-16 11:09:55 -0400

    Use all_countries db

--- aggregate_country_specific_rasters2.js
+++ aggregate_country_specific_rasters2.js
@@ -9,14 +9,16 @@ var config = require('./config');
 var aggregations_dir = config.aggregations_dir;
 var save_to_dir = config.save_to_dir;
 var save_raster_dir = config.save_raster_dir;
+var table_names = require('./shared/table_names');
 // var command = 'psql -l -t | cut -d'|' -f1 ';
 var command = "psql -lqt  | grep _";
 var pg_config = config.pg_config;
 
 // Get array of 3 letter iso country codes for which a db exists in postgres.
-country_table_names()
+table_names.country_table_names(pg_config)
 .then(tables => {
-  bluebird.each(Object.keys(tables), (country, i) => {
+
+  bluebird.each(Object.keys(tables).filter( t => { return t.match(/sle/)}), (country, i) => {
     return process_tables(country, tables[country]).then(() => {
       // Drop raster from table if exists
       drop_raster_table('all_countries', 'pop');
@@ -24,39 +26,6 @@ country_table_names()
   }, {concurrency: 1})
   .then(process.exit);
 });
-
-function country_table_names() {
-  return new Promise((resolve, reject) => {
-    var results = [];
-    console.log('About to query...');
-    pg.connect(pg_config, (err, client, done) => {
-      var st = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';";
-      var query = client.query(st);
-      // Stream results back one row at a time
-      query.on('row', (row) => {
-        results.push(row);
-      });
-      // After all data is returned, close connection and return results
-      query.on('end', () => {
-        resolve(results.filter(
-          r => { return r.table_name.match(/[a-z]{3}_\d/);}
-          )
-          .map(r => { return r.table_name})
-          .reduce((h, e) => {
-            var country = e.match(/[a-z]{3}/)[0];
-            if (h[country]) {
-              h[country].push(e);
-            } else {
-              h[country] = [e];
-            }
-            //h[country] = h[country] ? h[country].push(e) : [e];
-            return h;
-          }, {})
-        );
-      });
-    });
-  });
-}
 
 function tiff_file_name(content) {
   var ary = content.split(/\//);
@@ -67,9 +36,10 @@ function process_tables(country, country_tables) {
   return new Promise((resolve, reject) => {
     fetch_raster(country)
     .then(tif_file => {
+      console.log(tif_file, '!!!')
       if (!tif_file) { return resolve();}
-      console.log(country_tables, '!!!!');
       bluebird.each(country_tables, table => {
+        console.log(country_tables)
         return scan_raster(country, table, tif_file);
       }, {concurrency: 1})
       .then(() => {
@@ -90,6 +60,7 @@ function fetch_raster(country) {
     // as table named 'pop'
     var command = 'bash ./lib/fetch_and_process_raster.sh ' + country + ' ' + aggregations_dir + ' population ' + save_raster_dir;
     console.log(command);
+
     exec(command,{maxBuffer: 4096 * 2500}, (err, stdout, stderr) => {
       console.log(err, stdout, stderr)
       var tif_file = tiff_file_name(stdout);
@@ -111,51 +82,14 @@ function fetch_raster(country) {
   })
 }
 
-function customize_to_shapefile_source_specific_properties(st, admin_level, source) {
-  switch(source) {
-    case 'gadm2-8':
-      for(var i = 0; i <= admin_level; i++) {
-        st += '"' + admin_table + '"' + '.ID_' + i + ', ';
-      }
-      return st;
-      break;
-    // case 'santiblanko':
-    //   return st += '"' + admin_table + '"' + '.dpto as ID_1,' + '"' + admin_table + '"' + '.wcolgen02_ as ID_2, '
-
-    default:
-      return st;
-  }
-}
-
-function form_select_command(admin_table, shapefile_source) {
-  var admin_level = parseInt(admin_table.match(/\d/)[0]);
-  var st;
-  console.log(admin_level, shapefile_source, '****')
-  switch(shapefile_source) {
-    case 'gadm2-8':
-      st = 'SELECT gid, ST_Area(geom::geography)/1609.34^2 AS kilometers, ';
-      st = customize_to_shapefile_source_specific_properties(st, admin_table, admin_level, shapefile_source);
-      st += 'SUM((ST_SummaryStats(ST_Clip(rast, geom))).sum) FROM "' +
-      admin_table +
-      '" LEFT JOIN pop ON ST_Intersects("' + admin_table +
-      '".geom, pop.rast) GROUP BY gid;';
-      return st;
-      break
-    case 'santiblanko':
-      // return "SELECT ST_Area(col_2_santiblanko.wkb_geometry::geography)/1609.34^2 AS kilometers, dpto, wcolgen02_ as id_2, SUM((ST_SummaryStats(ST_Clip(rast, wkb_geometry, -9999))).sum) FROM col_2_santiblanko LEFT JOIN pop ON ST_Intersects(col_2_santiblanko.wkb_geometry, pop.rast) GROUP BY id_1, id_2, kilometers;"
-      return "SELECT ST_Area(col_2_santiblanko.wkb_geometry::geography)/1609.34^2 AS kilometers, dpto, wcolgen02_, SUM((ST_SummaryStats(ST_Clip(rast, wkb_geometry, -9999))).sum) FROM col_2_santiblanko LEFT JOIN pop ON ST_Intersects(col_2_santiblanko.wkb_geometry, pop.rast) GROUP BY dpto, wcolgen02_, kilometers;"
-    default:
-      return st;
-  }
-}
-
 function scan_raster(country, admin_table, tif_file) {
   return new Promise((resolve, reject) => {
     var results = [];
     config.database = country;
     pg.connect(pg_config, (err, client, done) => {
       var shapefile_source = admin_table.split('_')[admin_table.split('_').length-1];
-      var st = form_select_command(admin_table, shapefile_source);
+      var st = table_names.form_select_command(admin_table, shapefile_source, 'sum');
+
       var query = client.query(st);
 
       // Stream results back one row at a time
@@ -174,7 +108,7 @@ function scan_raster(country, admin_table, tif_file) {
         '^' + pop_sum +
         '^' + kilo_sum +
         '.json';
-
+        console.log(file);
         fs.writeFile(file,
         JSON.stringify(results), (err) => {
           if (err) console.log(err)
